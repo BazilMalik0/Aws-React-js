5d571729014ac98491a6c106ad6ce5a00f37d580
BazilMalik0 <bazilmalik01933@gmail.com>
2025-08-14 14:09:13 +0530

    update components

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -274,7 +274,7 @@ const Contact = () => {
               >
                 <span>Send Message</span>
                 <span className="btn-icon">
-                  <IoSend style={{ marginTop: "13px" }} />
+                  <IoSend />
                 </span>
               </button>
             </form>
